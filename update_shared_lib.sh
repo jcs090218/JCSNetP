cce5f1cc4b99966486533aad68e6a1b816f6782c
jenchieh <jcs090218@gmail.com>
2017-09-25 09:11:22 +0800

    add build shared library operation file. (Linux/bash)

---
+++ update_shared_lib.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#  ========================================================================
+#  $File: update_shared_lib.sh $
+#  $Date: 2017-04-12 10:57:20 $
+#  $Revision: $
+#  $Creator: Jen-Chieh Shen $
+#  $Notice: See LICENSE.txt for modification and distribution information
+#                    Copyright (c) 2017 by Shen, Jen-Chieh $
+#  ========================================================================
+
+# DESCRIPTION(jenchieh): preapre for running the program.
+
+set JCSNetP_DLL_RELEASE=JCSNetP.so
+set JCSNetP_DLL_DEBUG=JCSNetPd.so
+
+
